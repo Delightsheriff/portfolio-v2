c8ab0badaca9fe590f787e5de148af85f30c793b
Delight Sheriff <delightsheriff@gmail.com>
2024-12-25 00:02:54 +0100

    logo fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import Logo from "@/public/logo.png";
+import Logo from "../../public/logo.png";
 import UnmountStudio from "./Unmount";
 import { DarkModeToggle } from "./DarkmodeToggle";
 import MobileMenu from "./MobileMenu";
